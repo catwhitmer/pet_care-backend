02da9072d0972b47a1eb12a3870f78b4d82b3755
catwhitmer <catwhitmer@gmail.com>
2020-06-29 08:59:22 -0400

    namespaced the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :todos
-  resources :pets
-  resources :owners
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace :api do
+    namespace :v1 do 
+      resources :todos 
+      resources :pets
+      resources :owners
+    end
+  end
+  
 end
